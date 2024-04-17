7653d97adfd0c8673c5f1ab9facb9d64b6db1179
Bryan Dijkhuizen <bryandijkh@icloud.com>
2024-04-17 22:08:10 +0200

    Added styling to the join_match page

--- Frontend/lib/join_match.dart
+++ Frontend/lib/join_match.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class JoinMatch extends StatefulWidget {
   const JoinMatch({Key? key}) : super(key: key);
 
+  @override
   State<JoinMatch> createState() => _JoinMatchState();
 }
 
@@ -20,58 +21,86 @@ class _JoinMatchState extends State<JoinMatch> {
     'notJoined': ElevatedButton.styleFrom(
       backgroundColor: const Color(0xFFCD0612),
       foregroundColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10), // Adjust the radius as needed
+      ),
     ),
     'joined': ElevatedButton.styleFrom(
       backgroundColor: Colors.grey,
       foregroundColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10), // Adjust the radius as needed
+      ),
     ),
   };
+
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final buttonHeight = screenHeight * 0.15;
+    final startMatchPosition = screenHeight * 0.65;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Join Match'),
+        backgroundColor: const Color(0xFFCD0612),
+        foregroundColor: Colors.white,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              style: buttonStyles[player1Joined ? 'joined' : 'notJoined'],
-              onPressed: () {
-                setState(() {
-                  player1Joined = true;
-                });
-              },
-              child: const Text('Join as Player 1'),
-            ),
-            ElevatedButton(
-              style: buttonStyles[player2Joined ? 'joined' : 'notJoined'],
-              onPressed: () {
-                setState(() {
-                  player2Joined = true;
-                });
-              },
-              child: const Text('Join as Player 2'),
-            ),
-            ElevatedButton(
-              style: buttonStyles[markerJoined ? 'joined' : 'notJoined'],
-              onPressed: () {
-                setState(() {
-                  markerJoined = true;
-                });
-              },
-              child: const Text('Join as Marker'),
-            ),
-            if (allPlayersJoined())
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('./assets/images/bg.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: buttonHeight * 0.2), // Top padding
               ElevatedButton(
-                style: buttonStyles['notJoined'],
+                style: buttonStyles[player1Joined ? 'joined' : 'notJoined'],
                 onPressed: () {
-                  // Handle start match
+                  setState(() {
+                    player1Joined = true;
+                  });
                 },
-                child: const Text('Start Match'),
+                child: const Text('Join as Player 1'),
               ),
-          ],
+              SizedBox(height: buttonHeight * 0.15), // Space between buttons
+              ElevatedButton(
+                style: buttonStyles[player2Joined ? 'joined' : 'notJoined'],
+                onPressed: () {
+                  setState(() {
+                    player2Joined = true;
+                  });
+                },
+                child: const Text('Join as Player 2'),
+              ),
+              SizedBox(height: buttonHeight * 0.15), // Space between buttons
+              ElevatedButton(
+                style: buttonStyles[markerJoined ? 'joined' : 'notJoined'],
+                onPressed: () {
+                  setState(() {
+                    markerJoined = true;
+                  });
+                },
+                child: const Text('Join as Marker'),
+              ),
+              SizedBox(
+                  height: startMatchPosition -
+                      (3 *
+                          buttonHeight)), // Space to push buttons to 3/4 of the page
+              if (allPlayersJoined())
+                ElevatedButton(
+                  style: buttonStyles['notJoined'],
+                  onPressed: () {
+                    // Handle start match
+                  },
+                  child: const Text('Start Match'),
+                ),
+            ],
+          ),
         ),
       ),
     );
