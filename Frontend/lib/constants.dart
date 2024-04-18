f83d63913e1e372a6f20c0e8b110451356579a36
Bryan Dijkhuizen <bryandijkh@icloud.com>
2024-04-18 15:43:39 +0200

    Merge branch 'dev' of https://github.com/bryandijkhuizen/dartproject-s7 into dev

---
+++ Frontend/lib/constants.dart
@@ -0,0 +1,11 @@
+abstract class Constants {
+  static const String supabaseUrl = String.fromEnvironment(
+    'SUPABASE_URL',
+    defaultValue: '',
+  );
+
+  static const String supabaseAnonKey = String.fromEnvironment(
+    'SUPABASE_ANON_KEY',
+    defaultValue: '',
+  );
+}
