009d00dc80809d890386756a5b12ff4b3ab05319
Julian van Veen <julianvv2000@gmail.com>
2024-04-18 22:48:26 +0200

    Revert "Refactor: Auth route updated to new name"
    This reverts commit 8f276b8341e07a96f49a5fdb6b12c60c922c7907.

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -1,14 +1,8 @@
-import 'package:darts_application/constants.dart';
 import 'package:darts_application/features/app_router/app_router.dart';
 import 'package:darts_application/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() {
-  Supabase.initialize(
-    url: Constants.supabaseUrl,
-    anonKey: Constants.supabaseAnonKey,
-  );
   runApp(const MyApp());
 }
 
