7771c338ed6c1dd4ffa7b312de18065f24af93eb
Julian van Veen <julianvv2000@gmail.com>
2024-04-19 11:28:24 +0200

    Refactor: Updated theming and made login page scrollable on smaller screen sizes

--- Frontend/lib/features/auth/auth_view.dart
+++ Frontend/lib/features/auth/auth_view.dart
@@ -9,39 +9,92 @@ class AuthScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
 
+    Color getColor(Set<MaterialState> states) {
+      if (states.contains(MaterialState.hovered)) {
+        return theme.colorScheme.primary.withOpacity(0.4);
+      }
+
+      return theme.colorScheme.primary;
+    }
+
     return Scaffold(
-      backgroundColor: theme.colorScheme.background,
       body: Center(
         child: Container(
           constraints: const BoxConstraints(maxWidth: 600),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SupaEmailAuth(
-                onSignInComplete: (response) {},
-                onSignUpComplete: (response) {},
-                metadataFields: [
-                  MetaDataField(
-                    label: 'First name',
-                    key: 'first_name',
-                    prefixIcon: const Icon(Icons.assignment_ind),
-                    validator: ValidationBuilder(
-                            requiredMessage: 'Please fill in your first name')
-                        .maxLength(128, 'Last name is too long')
-                        .build(),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Dartin Molema',
+                  style: theme.textTheme.displaySmall,
+                ),
+                Text(
+                  'Sign in to become a pro',
+                  style: theme.textTheme.labelLarge,
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                SupaSocialsAuth(
+                  socialProviders: const [
+                    OAuthProvider.google,
+                    OAuthProvider.facebook,
+                    OAuthProvider.twitter,
+                  ],
+                  onSuccess: (response) {},
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 24.0),
+                  child: Divider(
+                    color: Colors.grey,
                   ),
-                  MetaDataField(
-                    label: 'Last name',
-                    key: 'last_name',
-                    prefixIcon: const Icon(Icons.assignment_ind),
-                    validator: ValidationBuilder(
-                            requiredMessage: 'Please fill in your last name')
-                        .maxLength(128, 'Last name is too long')
-                        .build(),
+                ),
+                Theme(
+                  data: theme.copyWith(
+                    elevatedButtonTheme: ElevatedButtonThemeData(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.resolveWith(getColor),
+                        foregroundColor:
+                            const MaterialStatePropertyAll(Colors.white),
+                        splashFactory: InkSplash.splashFactory,
+                      ),
+                    ),
                   ),
-                ],
-              ),
-            ],
+                  child: SupaEmailAuth(
+                    onSignInComplete: (response) {
+                      // app_router will handle redirecting the user inside the app
+                    },
+                    onSignUpComplete: (response) {
+                      // app_router will handle redirecting the user inside the app
+                    },
+                    metadataFields: [
+                      MetaDataField(
+                        label: 'First name',
+                        key: 'first_name',
+                        prefixIcon: const Icon(Icons.assignment_ind),
+                        validator: ValidationBuilder(
+                                requiredMessage:
+                                    'Please fill in your first name')
+                            .maxLength(128, 'Last name is too long')
+                            .build(),
+                      ),
+                      MetaDataField(
+                        label: 'Last name',
+                        key: 'last_name',
+                        prefixIcon: const Icon(Icons.assignment_ind),
+                        validator: ValidationBuilder(
+                                requiredMessage:
+                                    'Please fill in your last name')
+                            .maxLength(128, 'Last name is too long')
+                            .build(),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- Frontend/lib/theme.dart
+++ Frontend/lib/theme.dart
@@ -10,49 +10,90 @@ ThemeData darkTheme = ThemeData(
       .copyWith(
     brightness: Brightness.dark,
     secondary: const Color(0xFF2C4789),
-    background: const Color(0xFF060606),
+    background: const Color(0xFF101010),
+    surface: const Color(0xFF101010),
+    onSurface: Colors.white,
   ),
   inputDecorationTheme: InputDecorationTheme(
     filled: true,
-    fillColor: const Color(0xFFF9F9F9),
+    fillColor: const Color(0xFFFFFFFF),
     floatingLabelBehavior: FloatingLabelBehavior.never,
+    labelStyle: const TextStyle(
+      color: Color(0xFF6B6B6B),
+    ),
     border: OutlineInputBorder(
       borderRadius: BorderRadius.circular(10),
     ),
     iconColor: Colors.black,
     prefixIconColor: Colors.black,
-    labelStyle: const TextStyle(
-      color: Color(0xFF6B6B6B),
-    ),
+    suffixIconColor: Colors.black,
   ),
   textTheme: TextTheme(
     displayLarge: GoogleFonts.poppins(
-        fontSize: 94, fontWeight: FontWeight.w300, letterSpacing: -1.5),
+      fontSize: 94,
+      fontWeight: FontWeight.w300,
+      letterSpacing: -1.5,
+    ),
     displayMedium: GoogleFonts.poppins(
-        fontSize: 59, fontWeight: FontWeight.w300, letterSpacing: -0.5),
-    displaySmall:
-        GoogleFonts.poppins(fontSize: 47, fontWeight: FontWeight.w400),
+      fontSize: 59,
+      fontWeight: FontWeight.w300,
+      letterSpacing: -0.5,
+    ),
+    displaySmall: GoogleFonts.poppins(
+      fontSize: 47,
+      fontWeight: FontWeight.w400,
+    ),
     headlineMedium: GoogleFonts.poppins(
-        fontSize: 33, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-    headlineSmall:
-        GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.w400),
+      fontSize: 33,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0.25,
+    ),
+    headlineSmall: GoogleFonts.poppins(
+      fontSize: 24,
+      fontWeight: FontWeight.w400,
+    ),
     titleLarge: GoogleFonts.poppins(
-        fontSize: 20, fontWeight: FontWeight.bold, letterSpacing: 0.15),
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      letterSpacing: 0.15,
+    ),
     titleMedium: GoogleFonts.poppins(
-        fontSize: 16, fontWeight: FontWeight.bold, letterSpacing: 0.15),
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      letterSpacing: 0.15,
+    ),
     titleSmall: GoogleFonts.poppins(
-        fontSize: 14, fontWeight: FontWeight.bold, letterSpacing: 0.1),
+      fontSize: 14,
+      fontWeight: FontWeight.bold,
+      letterSpacing: 0.1,
+    ),
     bodyLarge: GoogleFonts.poppins(
-        fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0.5,
+    ),
     bodyMedium: GoogleFonts.poppins(
-        fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0.25,
+    ),
     labelLarge: GoogleFonts.poppins(
-        fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      letterSpacing: 1.25,
+    ),
     bodySmall: GoogleFonts.poppins(
-        fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0.4,
+    ),
     labelSmall: GoogleFonts.poppins(
-        fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
+      fontSize: 10,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 1.5,
+    ),
   ),
+  useMaterial3: true,
 );
 
 ThemeData lightTheme = ThemeData();
