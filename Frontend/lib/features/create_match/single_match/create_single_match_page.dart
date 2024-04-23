d3b0af4cf839e4a8a8d43de4a6b96d9ff83a194b
wtzb <dev@wietzeb.nl>
2024-04-23 10:29:07 +0200

    Add 2nd column

--- Frontend/lib/features/create_match/single_match/create_single_match_page.dart
+++ Frontend/lib/features/create_match/single_match/create_single_match_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:darts_application/components/input_fields/time_picker.dart';
 import 'package:darts_application/components/input_fields/date_picker.dart';
@@ -28,47 +29,87 @@ class _CreateSingleMatchPageState extends State<CreateSingleMatchPage> {
         title: const Text('Create Single Match'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
+        padding: EdgeInsets.all(20.0),
+        child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
-              'Match Name (optional)',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              controller: _matchNameController,
-              decoration: const InputDecoration(
-                hintText: 'Enter the name of the match',
-                border: OutlineInputBorder(),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Match Name (optional)',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: _matchNameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Enter the name of the match',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Date',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 10),
+                  const DatePicker(),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Time',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 10),
+                  const TimePicker(),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Location',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: _locationController,
+                    decoration: const InputDecoration(
+                      labelText: 'Enter the location of the match',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 20),
-            const Text(
-              'Date',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            const DatePicker(),
-            const SizedBox(height: 20),
-            const Text(
-              'Time',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            const TimePicker(),
-            const SizedBox(height: 20),
-            const Text(
-              'Location',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              controller: _locationController,
-              decoration: const InputDecoration(
-                hintText: 'Enter the location of the match',
-                border: OutlineInputBorder(),
+            const SizedBox(width: 20),
+            const Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Duration (best of)',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: <Widget>[
+                      Text(
+                        'FIRST',
+                        style: TextStyle(fontSize: 20.0),
+                      ),
+                      Text(
+                        'SECOND',
+                        style: TextStyle(fontSize: 20.0),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 20),
+                  Text(
+                    'Match type',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  
+                  SizedBox(height: 10),
+                ],
               ),
             ),
           ],
