fd059ed5452db12362312978a712a2e00b49c9d0
wtzb <dev@wietzeb.nl>
2024-04-23 13:11:03 +0200

    Add selector page & routing

--- Frontend/lib/features/app_router/app_router.dart
+++ Frontend/lib/features/app_router/app_router.dart
@@ -6,6 +6,9 @@ import 'package:darts_application/helpers.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+// import '../create_match/single_match/create_single_match_page.dart';
+import '../create_match/create_selector.dart';
+
 Widget getPlaceholderComponent(
     String currentRoute, List<String> routes, BuildContext context) {
   return Center(
@@ -178,15 +181,16 @@ final router = GoRouter(
                   GoRoute(
                     path: '/matches',
                     builder: (context, state) {
+                      return const CreateSelector();
                       // Ignore this for now
-                      return getPlaceholderComponent(
-                          '/matches',
-                          [
-                            '/',
-                            '/matches',
-                            '/settings',
-                          ],
-                          context);
+                      // return getPlaceholderComponent(
+                      //     '/matches',
+                      //     [
+                      //       '/',
+                      //       '/matches',
+                      //       '/settings',
+                      //     ],
+                      //     context);
                     },
                   ),
                 ],
