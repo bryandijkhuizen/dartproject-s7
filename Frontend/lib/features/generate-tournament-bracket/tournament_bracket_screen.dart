d12fc638d2e828f2f8eba5d64d1158ee684924f4
LuukOttens <luukottens2000@gmail.com>
2024-04-25 14:47:57 +0200

    Make createRounds

--- Frontend/lib/features/generate-tournament-bracket/tournament_bracket_screen.dart
+++ Frontend/lib/features/generate-tournament-bracket/tournament_bracket_screen.dart
@@ -22,7 +22,9 @@ class TournamentBracketScreen extends StatelessWidget {
     "Danny Noppert",
   ];
 
-  TournamentBracketScreen({super.key});
+  TournamentBracketScreen({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -32,8 +34,6 @@ class TournamentBracketScreen extends StatelessWidget {
     );
     var titleMediumWhite =
         theme.textTheme.titleMedium?.copyWith(color: Colors.white);
-    var bodyMediumWhite =
-        theme.textTheme.bodyMedium?.copyWith(color: Colors.white);
 
     const String avatarUrl = "assets/images/avatar_placeholder.png";
     List<Player> players = [];
@@ -44,7 +44,7 @@ class TournamentBracketScreen extends StatelessWidget {
 
     return Center(
       child: Container(
-        width: 900.00,
+        width: 1200.00,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -60,7 +60,6 @@ class TournamentBracketScreen extends StatelessWidget {
               margin: EdgeInsets.only(bottom: 20.0),
               child: Text(
                 "Review the proposed matches.",
-                style: bodyMediumWhite,
               ),
             ),
             Container(
@@ -70,8 +69,9 @@ class TournamentBracketScreen extends StatelessWidget {
                 style: titleMediumWhite,
               ),
             ),
-            TournamentBracket(
-                players: players, bodyMediumWhite: bodyMediumWhite),
+            TournamentBrackets(
+              players: players,
+            ),
           ],
         ),
       ),
@@ -86,51 +86,103 @@ class Player {
   Player(this.name, this.avatarUrl);
 }
 
-class TournamentBracket extends StatelessWidget {
-  const TournamentBracket({
+class TournamentBrackets extends StatelessWidget {
+  const TournamentBrackets({
     super.key,
     required this.players,
-    required this.bodyMediumWhite,
   });
 
   final List<Player> players;
-  final TextStyle? bodyMediumWhite;
+  final String avatarUrl = "assets/images/avatar_placeholder.png";
+
+  List<Widget> createRounds(List<Player> players, int round,
+      {bool fillInPlayers = false}) {
+    List<Widget> tournamentBracket = [];
+    List<MatchupCard> matches =
+        createMatches(players, fillInPlayers: fillInPlayers);
+
+    final ScrollController _scrollController = ScrollController();
+
+    Widget roundLayout = Column(
+      children: [
+        Text(
+          "Round ${round.toString()}",
+        ),
+        Container(
+          color: Colors.white,
+          height: 500,
+          width: 400,
+          child: Scrollbar(
+            controller: _scrollController,
+            child: ListView.builder(
+              controller: _scrollController,
+              scrollDirection: Axis.vertical,
+              itemCount: matches.length,
+              itemBuilder: (context, index) {
+                return matches[index];
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+
+    tournamentBracket.add(roundLayout);
+
+    if (matches.length > 1) {
+      List<Player> nextRoundPlayers = [];
+      for (var i = 1; i <= matches.length; i++) {
+        nextRoundPlayers.add(Player("Winner match $i", avatarUrl));
+      }
+
+      List<Widget> nextRoundBracket =
+          createRounds(nextRoundPlayers, ++round, fillInPlayers: true);
+      tournamentBracket.addAll(nextRoundBracket);
+    }
+
+    return tournamentBracket;
+  }
+
+  List<MatchupCard> createMatches(List<Player> players,
+      {bool fillInPlayers = false}) {
+    List<MatchupCard> matches = [];
+    Player placeholderPlayer = Player("", avatarUrl);
+    var amountOfPlayers = players.length;
+    var amountOfMatches = (amountOfPlayers / 2).ceil();
+
+    while (amountOfMatches >= 1) {
+      Player firstPlayer;
+      Player secondPlayer;
+
+      if (fillInPlayers) {
+        firstPlayer =
+            players.isNotEmpty ? players.removeAt(0) : placeholderPlayer;
+        secondPlayer =
+            players.isNotEmpty ? players.removeAt(0) : placeholderPlayer;
+      } else {
+        firstPlayer = placeholderPlayer;
+        secondPlayer = placeholderPlayer;
+      }
+
+      matches.add(MatchupCard(
+        firstPlayer: firstPlayer,
+        secondPlayer: secondPlayer,
+      ));
+
+      amountOfMatches--;
+    }
+
+    return matches;
+  }
 
   @override
   Widget build(BuildContext context) {
-    final ScrollController _scrollController = ScrollController();
+    List<Widget> rounds = createRounds(players, 1, fillInPlayers: false);
 
-    List<MatchupCard> matches = [];
-    Player firstPlayer = Player("", "");
-    var playerCount = 0;
-
-    for (var player in players) {
-      if (playerCount == 0) {
-        firstPlayer = player;
-        playerCount = 1;
-      } else if (playerCount == 1) {
-        matches.add(MatchupCard(
-            firstPlayer: firstPlayer,
-            secondPlayer: player,
-            bodyMediumWhite: bodyMediumWhite));
-        playerCount = 0;
-      }
-    }
-
-    return Container(
-      color: Colors.white,
-      height: 400,
-      width: 500,
-      child: Scrollbar(
-        controller: _scrollController,
-        child: ListView.builder(
-          controller: _scrollController,
-          scrollDirection: Axis.vertical,
-          itemCount: matches.length,
-          itemBuilder: (context, index) {
-            return matches[index];
-          },
-        ),
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: rounds,
       ),
     );
   }
@@ -139,13 +191,11 @@ class TournamentBracket extends StatelessWidget {
 class MatchupCard extends StatelessWidget {
   final Player firstPlayer;
   final Player secondPlayer;
-  final TextStyle? bodyMediumWhite;
 
   const MatchupCard({
     super.key,
     required this.firstPlayer,
     required this.secondPlayer,
-    required this.bodyMediumWhite,
   });
 
   @override
@@ -157,12 +207,9 @@ class MatchupCard extends StatelessWidget {
         children: [
           PlayerCard(
             player: firstPlayer,
-            bodyMediumWhite: bodyMediumWhite,
           ),
-          // Spacer(),
           PlayerCard(
             player: secondPlayer,
-            bodyMediumWhite: bodyMediumWhite,
           ),
         ],
       ),
@@ -174,31 +221,21 @@ class PlayerCard extends StatelessWidget {
   const PlayerCard({
     super.key,
     required this.player,
-    required this.bodyMediumWhite,
   });
 
   final Player player;
-  final TextStyle? bodyMediumWhite;
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Row(
-        // crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SizedBox(width: 8),
           CircleAvatar(
               radius: 20, backgroundImage: AssetImage(player.avatarUrl)),
           SizedBox(width: 8),
-          // SvgPicture.asset(
-          //   width: 50,
-          //   height: 50,
-          //   avatar,
-          //   semanticsLabel: 'Avatar from player',
-          // ),
           Text(
             player.name,
-            // style: bodyMediumWhite,
           ),
           SizedBox(width: 8),
         ],
