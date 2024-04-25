f112bf009d56bcbb7d8772631d254a658639d13e
Julian van Veen <julianvv2000@gmail.com>
2024-04-25 20:06:48 +0200

    Feat: Added re-usable save and cancel button

---
+++ Frontend/lib/components/form_save_button.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class FormSaveButton extends StatelessWidget {
+  const FormSaveButton(
+      {super.key, required this.onCancel, required this.onSave});
+
+  final void Function()? onCancel;
+  final void Function()? onSave;
+
+  MaterialStateProperty<Color?> getMaterialStateOverlayColor(ThemeData theme) =>
+      MaterialStateProperty.resolveWith(
+        (states) => theme.colorScheme.onPrimary.withOpacity(0.1),
+      );
+
+  final BorderRadius leftButtonBorderRadius = const BorderRadius.only(
+    topLeft: Radius.circular(10),
+    bottomLeft: Radius.circular(10),
+  );
+
+  final BorderRadius rightButtonBorderRadius = const BorderRadius.only(
+    topRight: Radius.circular(10),
+    bottomRight: Radius.circular(10),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    return Row(
+      children: [
+        Expanded(
+          child: ElevatedButton(
+            style: ButtonStyle(
+              overlayColor: getMaterialStateOverlayColor(theme),
+              shape: MaterialStateProperty.resolveWith(
+                (states) => RoundedRectangleBorder(
+                  borderRadius: leftButtonBorderRadius,
+                ),
+              ),
+            ),
+            onPressed: () {
+              onCancel?.call();
+            },
+            child: const Text('Cancel'),
+          ),
+        ),
+        Expanded(
+          child: ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.resolveWith(
+                (states) => theme.colorScheme.secondary,
+              ),
+              overlayColor: getMaterialStateOverlayColor(theme),
+              shape: MaterialStateProperty.resolveWith(
+                (states) => RoundedRectangleBorder(
+                  borderRadius: rightButtonBorderRadius,
+                ),
+              ),
+            ),
+            onPressed: () {
+              onCancel?.call();
+            },
+            child: const Text('Save'),
+          ),
+        ),
+      ],
+    );
+  }
+}
