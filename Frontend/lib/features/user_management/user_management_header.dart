42d95ed025a6d05ef8eb47038c56d6180faac3b5
KevinHerrema <kevin.herrema@student.nhlstenden.com>
2024-05-07 12:40:36 +0200

    Feat: role assigment working for system wide roles
    Role assignment now works for all the system roles and the filter options in the user selection page now also work.

--- Frontend/lib/features/user_management/user_management_header.dart
+++ Frontend/lib/features/user_management/user_management_header.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class UserManagementHeader extends StatelessWidget {
-  const UserManagementHeader({super.key});
+  UserManagementHeader(this.supabaseClient, {Key? key, required this.onSearch})
+      : super(key: key) {
+    _searchController = TextEditingController();
+  }
+  final void Function(String? searchedName,String? searchedRole ) onSearch;
+
+  
+  final SupabaseClient supabaseClient;
+  String? selectedRole;
+  late TextEditingController _searchController;
+  Future loadRoles() {
+    return Supabase.instance.client.rpc("get_role_names");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +41,7 @@ class UserManagementHeader extends StatelessWidget {
                     children: [
                       Text("Name"),
                       TextField(
+                        controller: _searchController,
                         decoration: InputDecoration(
                           border: OutlineInputBorder(),
                           hintText: 'Enter a search term',
@@ -37,24 +51,54 @@ class UserManagementHeader extends StatelessWidget {
                   ),
                 ),
               ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Role"),
-                    TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Enter a search term',
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              FutureBuilder(
+                  future: loadRoles(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      if (snapshot.hasError) {
+                        return const Text(
+                            "something went wrong while getting the roles");
+                      }
+                      if (snapshot.hasData) {
+                        List<dynamic> roles = snapshot.data as List<
+                            dynamic>; // Assuming the data is a list of roles
+
+                        List<DropdownMenuEntry<dynamic>> dropdownMenuEntries =
+                            roles.map((role) {
+                          return DropdownMenuEntry<dynamic>(
+                            value:
+                                role["role_name"].toString(), // Assuming roles are strings
+                            label: role["role_name"].toString(),
+                          );
+                        }).toList();
+
+                        return Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Role"),
+                              DropdownMenu(
+                                onSelected: (value) {
+                                  selectedRole = value;
+                                },
+                                dropdownMenuEntries: dropdownMenuEntries,
+                              ),
+                            ],
+                          ),
+                        );
+                      }
+                    }
+                    return CircularProgressIndicator();
+                  }),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: FilledButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    // Get the search term from the text field
+                    String searchedName = _searchController.text;
+                  // Trigger the callback with the search term
+                    onSearch(searchedName, selectedRole);
+                  },
                   child: const Text("Search"),
                 ),
               )
