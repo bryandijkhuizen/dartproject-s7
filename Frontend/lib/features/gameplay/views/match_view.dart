1cd4880aec451e2c1327279170715b9446a0f36e
LegendaryBosma <64129961+LegendaryBosma@users.noreply.github.com>
2024-05-13 14:55:46 +0200

    Numpad added

---
+++ Frontend/lib/features/gameplay/views/match_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MatchView extends StatelessWidget {
+  const MatchView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
