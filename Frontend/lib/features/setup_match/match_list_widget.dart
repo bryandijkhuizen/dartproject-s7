58625b1d1f16066203c0082f698e992f5dccb577
Bryan Dijkhuizen <bryandijkh@icloud.com>
2024-05-15 10:48:12 +0200

    Update match_list_widget.dart

--- Frontend/lib/features/setup_match/match_list_widget.dart
+++ Frontend/lib/features/setup_match/match_list_widget.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unrelated_type_equality_checks
+// ignore_for_file: unrelated_type_equality_checks, use_build_context_synchronously
 
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
