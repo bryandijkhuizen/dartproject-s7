720b1803d0b29389435d25c01ecad9db424231e9
KevinHerrema <71255086+KevinHerrema@users.noreply.github.com>
2024-05-29 15:50:32 +0200

    Revert "v0.1.0+2"

--- supabase/migrations/20240529131256_added_phonenumber_and_email_to_clubs.sql
+++
@@ -1,2 +0,0 @@
-alter table "public"."club" add column "phone_number" text not null default 'No number found';
-alter table "public"."club" add column "email_address" text not null default 'No email address found';
