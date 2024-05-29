0de407cb5da58293fcf7cd86130dcbcc9fa3354a
KevinHerrema <kevin.herrema@student.nhlstenden.com>
2024-05-29 14:22:19 +0200

    Feat: added members management tables and functionality

--- Frontend/lib/features/club_management/views/current_members_manager_view.dart
+++ Frontend/lib/features/club_management/views/current_members_manager_view.dart
@@ -15,58 +15,115 @@ class CurrentMembersManagerView extends StatefulWidget {
 
 class _CurrentMembersManagerViewState extends State<CurrentMembersManagerView> {
   late MembersManager manager;
-  @override
-  void initState() {
+
+  void loadData() {
     UserStore userStore = context.read();
     int clubId = userStore.permissions
         .getClubIdByPermission(PermissionList.manageClubMembers);
     manager = MembersManager(Supabase.instance.client, clubId);
-    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
+    loadData();
     return FutureBuilder(
-        future: manager.members,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Text('something went wrong');
-          }
-          if (snapshot.hasData) {
-            Map<String, String> memberlist = snapshot.data!;
-            return DataTable(
-              headingRowColor:
-                  MaterialStatePropertyAll(theme.colorScheme.primary),
-              headingTextStyle: const TextStyle(color: Colors.white),
-              columnSpacing: 10,
-              columns: const [
-                DataColumn(
-                  label: Text('Name'),
-                ),
-                DataColumn(
-                  label: Text(''),
-                ),
-              ],
-              rows: [
-                for (MapEntry<String, String> member in memberlist.entries)
-                  DataRow(
-                    color: MaterialStatePropertyAll(Colors.grey.shade300),
-                    cells: [
-                      DataCell(
-                        Text(member.value, style: TextStyle(color: Colors.black)),
-                      ),
-                      DataCell(FilledButton(
-                          onPressed: () {
-                            print("monkey");
-                          },
-                          child: const Text('Remove')))
-                    ],
+      future: manager.members,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text('something went wrong');
+        }
+        if (snapshot.hasData) {
+          Map<String, String> memberlist = snapshot.data!;
+          return Stack(
+            children: [
+              DataTable(
+                headingRowColor:
+                    WidgetStateProperty.all(theme.colorScheme.primary),
+                headingTextStyle: const TextStyle(color: Colors.white),
+                columnSpacing: 10,
+                columns: const [
+                  DataColumn(
+                    label: Text('Name'),
                   ),
-              ],
-            );
-          }
-          return const CircularProgressIndicator();
-        });
+                  DataColumn(
+                    label: Text("")
+                  ),
+                ],
+                rows: [
+                  for (MapEntry<String, String> member in memberlist.entries)
+                    DataRow(
+                      color: WidgetStateProperty.all(Colors.grey.shade300),
+                      cells: [
+                        DataCell(
+                          Text(member.value,
+                              style: const TextStyle(color: Colors.black)),
+                        ),
+                        DataCell(
+                          FilledButton(
+                            onPressed: () async {
+                              // show the AlertDialog
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: const Text("Notice",
+                                        style: TextStyle(color: Colors.black)),
+                                    content: const Text(
+                                      "Are you sure you want to remove this member from your club?",
+                                      style: TextStyle(color: Colors.black),
+                                    ),
+                                    actions: [
+                                      FilledButton(
+                                        child: const Text("Confirm"),
+                                        onPressed: () async {
+                                          await Supabase.instance.client.rpc(
+                                              "delete_user_from_club",
+                                              params: {
+                                                'current_user_id': member.key,
+                                                'current_club_id':
+                                                    manager.clubId
+                                              });
+                                          Navigator.of(context).pop();
+                                          setState(() {});
+                                        },
+                                      ),
+                                      FilledButton(
+                                        child: const Text("Cancel"),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            },
+                            child: const Text('Remove'),
+                          ),
+                        ),
+                      ],
+                    ),
+                ],
+              ),
+              Positioned(
+                top: 8,
+                right: 8,
+                child: IconButton(
+                  onPressed: () {
+                    setState(() {});
+                  },
+                  icon: Icon(
+                    Icons.refresh,
+                    color: theme.colorScheme.onPrimary,
+                  ),
+                ),
+              ),
+            ],
+          );
+        }
+        return const CircularProgressIndicator();
+      },
+    );
   }
 }
