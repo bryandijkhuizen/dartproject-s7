3d8a7bfc1cf0f453ac3c68fe2f2605c744b9c7c6
wtzb <dev@wietzeb.nl>
2024-05-31 12:47:39 +0200

    Implement post retrieval from database

--- Frontend/lib/features/club_page/views/club_details_view.dart
+++ Frontend/lib/features/club_page/views/club_details_view.dart
@@ -6,7 +6,8 @@ import 'recent_scores_view.dart';
 import 'club_posts_view.dart';
 
 class ClubDetailsView extends StatefulWidget {
-  const ClubDetailsView({super.key, required String clubId});
+  final String clubId;
+  const ClubDetailsView({super.key, required this.clubId});
 
   @override
   ClubDetailsViewState createState() => ClubDetailsViewState();
@@ -23,58 +24,80 @@ class ClubDetailsViewState extends State<ClubDetailsView> {
         centerTitle: true,
       ),
       body: GenericScreen(
-        child: Column(
-          children: [
-            Text("<Club image here>"),
-            const SizedBox(
-              height: 24,
-            ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      'Posts',
-                      style: theme.textTheme.titleMedium,
-                    ),
-                    const Card(
-                      elevation: 0,
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(
-                          vertical: 16.0,
-                        ),
-                        child: Column(
-                          children: [ClubPostsView()],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 12,
-                    ),
-                    Text(
-                      'Club Members',
-                      style: theme.textTheme.titleMedium,
-                    ),
-                    const Card(
-                      elevation: 0,
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(
-                          vertical: 16.0,
-                        ),
-                        child: Column(
-                          children: [ClubMembersView()],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 12,
-                    ),
-                  ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text("<Club image here>"),
+              const SizedBox(
+                height: 24,
+              ),
+              Text(
+                'Posts',
+                style: theme.textTheme.titleMedium,
+              ),
+              Card(
+                elevation: 0,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 16.0,
+                  ),
+                  child: SizedBox(
+                    height: 300,
+                    child: ClubPostsView(clubId: widget.clubId),
+                  ),
                 ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 12,
+              ),
+              Text(
+                'Club Members',
+                style: theme.textTheme.titleMedium,
+              ),
+              const Card(
+                elevation: 0,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 16.0,
+                  ),
+                  child: ClubMembersView(),
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Text(
+                'Upcoming Matches',
+                style: theme.textTheme.titleMedium,
+              ),
+              const Card(
+                elevation: 0,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 16.0,
+                  ),
+                  child: UpcomingMatchesView(),
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Text(
+                'Recent Scores',
+                style: theme.textTheme.titleMedium,
+              ),
+              const Card(
+                elevation: 0,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 16.0,
+                  ),
+                  child: RecentScoresView(),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- Frontend/lib/features/club_page/views/club_posts_view.dart
+++ Frontend/lib/features/club_page/views/club_posts_view.dart
@@ -1,22 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../widgets/post_card.dart';
+import 'package:darts_application/models/club_post.dart';
 
 class ClubPostsView extends StatefulWidget {
-  const ClubPostsView({super.key});
+  final String clubId;
+  const ClubPostsView({super.key, required this.clubId});
 
   @override
   ClubPostsViewState createState() => ClubPostsViewState();
 }
 
 class ClubPostsViewState extends State<ClubPostsView> {
+  late Future<List<ClubPost>> futureClubPosts;
+
+  @override
+  void initState() {
+    super.initState();
+    futureClubPosts = fetchPosts();
+  }
+
+  Future<List<ClubPost>> fetchPosts() async {
+    try {
+      final postResponse =
+          await Supabase.instance.client.from('club_post').select().eq('club_id', widget.clubId);;
+      final List<dynamic> postData = postResponse as List<dynamic>;
+      List<ClubPost> fetchedPosts = postData.map((row) {
+        return ClubPost.fromJson(row);
+      }).toList();
+      
+      return fetchedPosts;
+    } catch (e) {
+      print('Error fetching data: $e');
+      return [];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        PostCard(postContent: 'Lorem ipsum'),
-        SizedBox(height: 20),
-        PostCard(postContent: 'Lorem ipsum'),
-      ],
+    return FutureBuilder<List<ClubPost>>(
+      future: futureClubPosts,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(child: Text('No posts available'));
+        } else {
+          final clubPosts = snapshot.data!;
+          return ListView.builder(
+            itemCount: clubPosts.length,
+            itemBuilder: (context, index) {
+              final post = clubPosts[index];
+              return PostCard(
+                post: post,
+              );
+            },
+          );
+        }
+      },
     );
   }
 }
