20637eebd31875847e7fbdc0020230e8672cfa68
wtzb <dev@wietzeb.nl>
2024-05-31 19:57:27 +0200

    Add post detail screen

--- Frontend/lib/features/club_page/widgets/post_card.dart
+++ Frontend/lib/features/club_page/widgets/post_card.dart
@@ -3,58 +3,62 @@ import 'package:darts_application/models/club_post.dart';
 
 class PostCard extends StatelessWidget {
   final ClubPost post;
+  final VoidCallback onTap;
 
-  const PostCard({super.key, required this.post});
+  const PostCard({super.key, required this.post, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(8.0),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.0),
-      ),
-      child: Stack(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(15.0),
-            child: Image.network(
-              post.imageUrl!,
-              width: double.infinity,
-              height: 200,
-              fit: BoxFit.cover,
-              errorBuilder: (context, error, stackTrace) {
-                return Container(
-                  width: double.infinity,
-                  height: 200,
-                  color: Colors.grey,
-                  child: const Icon(Icons.broken_image, size: 50),
-                );
-              },
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.5),
-                borderRadius: const BorderRadius.vertical(bottom: Radius.circular(15.0)),
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        margin: const EdgeInsets.all(8.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: Stack(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image.network(
+                post.imageUrl!,
+                width: double.infinity,
+                height: 200,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) {
+                  return Container(
+                    width: double.infinity,
+                    height: 200,
+                    color: Colors.grey,
+                    child: const Icon(Icons.broken_image, size: 50),
+                  );
+                },
               ),
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                post.title,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.8),
+                  borderRadius: const BorderRadius.vertical(bottom: Radius.circular(15.0)),
                 ),
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  post.title,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
