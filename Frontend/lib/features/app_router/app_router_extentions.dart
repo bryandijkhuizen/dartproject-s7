c04a8d7af48177a1fc7ca7dfe45b44f0dd137f4c
Julian van Veen <julianvv2000@gmail.com>
2024-06-01 14:48:18 +0200

    Fix: Removed duplicate code and fixed typo in app_router_extensions

--- Frontend/lib/features/app_router/app_router_extentions.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-
-extension RouterExtentions on BuildContext {
-  void goBack(String fallbackLocation) {
-    canPop() ? pop() : go(fallbackLocation);
-  }
-}
