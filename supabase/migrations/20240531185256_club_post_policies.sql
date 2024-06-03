7287c3c86be3820146f1a5f0f8840b70b57d4a4e
wtzb <dev@wietzeb.nl>
2024-06-03 21:39:36 +0200

    Add initial policies

---
+++ supabase/migrations/20240531185256_club_post_policies.sql
@@ -0,0 +1,20 @@
+create policy "Users can view posts"
+on "public"."match"
+as permissive
+for select
+to public
+using (true);
+
+create policy "Users can create new posts"
+on "public"."club_post"
+as permissive
+for insert
+to public
+with check (true);
+
+create policy "Users can edit posts"
+on "public"."club_post"
+as permissive
+for update
+to public
+using (true);
