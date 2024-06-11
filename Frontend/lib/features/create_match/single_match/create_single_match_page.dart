b859f5b409a6fc58f7c29fd4af8d802ce203c7e8
Bryan Dijkhuizen <bryandijkh@icloud.com>
2024-06-11 09:36:25 +0200

    Function for is_friendly

--- Frontend/lib/features/create_match/single_match/create_single_match_page.dart
+++ Frontend/lib/features/create_match/single_match/create_single_match_page.dart
@@ -364,7 +364,8 @@ class _CreateSingleMatchPageState extends State<CreateSingleMatchPage> {
             style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
           ),
         ),
-        PlayerSelector(onSelectionChanged: updateSelectedPlayer),
+        PlayerSelector(
+            onSelectionChanged: updateSelectedPlayer, isFriendly: isFriendly),
         const SizedBox(height: 20),
         Center(
           child: ElevatedButton(
