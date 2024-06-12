efd38f6158fbea3d84879fc1b63c46ea0ef18134
wtzb <dev@wietzeb.nl>
2024-06-12 20:48:41 +0200

    Implement NewsFeed in club page

--- Frontend/lib/features/club_page/views/club_details_view.dart
+++ Frontend/lib/features/club_page/views/club_details_view.dart
@@ -6,6 +6,10 @@ import 'package:darts_application/features/club_page/views/recent_scores_view.da
 import 'package:darts_application/features/club_page/views/club_posts_view.dart';
 import 'package:darts_application/features/club_management/views/current_members_manager_view.dart';
 import 'package:darts_application/features/club_management/views/new_members_manager_view.dart';
+import 'package:darts_application/features/home/news_feed_scroll_view.dart';
+import 'package:darts_application/stores/news_feed_store.dart';
+import 'package:provider/provider.dart';
+import 'package:darts_application/services/post_service.dart';
 
 class ClubDetailsView extends StatefulWidget {
   final String clubId;
@@ -22,119 +26,125 @@ class ClubDetailsViewState extends State<ClubDetailsView> {
     ThemeData theme = Theme.of(context);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Club Details'),
-        centerTitle: true,
-      ),
-      body: GenericScreen(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text("<Club image here>"),
-              const SizedBox(
-                height: 24,
-              ),
-              Text(
-                'Posts',
-                style: theme.textTheme.titleMedium,
-              ),
-              Card(
-                elevation: 0,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 16.0,
-                  ),
-                  child: ClubPostsView(clubId: widget.clubId, limit: 3),
-                ),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              Text(
-                'Club Members',
-                style: theme.textTheme.titleMedium,
-              ),
-              const Card(
-                elevation: 0,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                    vertical: 16.0,
-                  ),
-                  child: ClubMembersView(),
-                ),
-              ),              const SizedBox(
-                height: 12,
-              ),
-                            Card(
-                elevation: 0,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const CurrentMembersManagerView(),
-                            ),
-                          );
-                        },
-                        child: const Text('Current Members'),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const NewMembersManagerView(),
-                            ),
-                          );
-                        },
-                        child: const Text('New Members'),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              Text(
-                'Upcoming Matches',
-                style: theme.textTheme.titleMedium,
-              ),
-              const Card(
-                elevation: 0,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                    vertical: 16.0,
-                  ),
-                  child: UpcomingMatchesView(),
-                ),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              Text(
-                'Recent Scores',
-                style: theme.textTheme.titleMedium,
-              ),
-              const Card(
-                elevation: 0,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                    vertical: 16.0,
-                  ),
-                  child: RecentScoresView(),
-                ),
-              ),
-            ],
-          ),
+        appBar: AppBar(
+          title: const Text('Club Details'),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: Provider(
+          create: (_) => NewsFeedStore(
+            context.read<PostService>(),
+          ),
+          child: GenericScreen(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text("<Club image here>"),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  Text(
+                    'Posts',
+                    style: theme.textTheme.titleMedium,
+                  ),
+                  const Card(
+                    elevation: 0,
+                    child: SizedBox(
+                        height: 500,
+                        child: NewsFeedScrollView(
+                          enableShowMore: true,
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    'Club Members',
+                    style: theme.textTheme.titleMedium,
+                  ),
+                  const Card(
+                    elevation: 0,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 16.0,
+                      ),
+                      child: ClubMembersView(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Card(
+                    elevation: 0,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      const CurrentMembersManagerView(),
+                                ),
+                              );
+                            },
+                            child: const Text('Current Members'),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      const NewMembersManagerView(),
+                                ),
+                              );
+                            },
+                            child: const Text('New Members'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    'Upcoming Matches',
+                    style: theme.textTheme.titleMedium,
+                  ),
+                  const Card(
+                    elevation: 0,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 16.0,
+                      ),
+                      child: UpcomingMatchesView(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    'Recent Scores',
+                    style: theme.textTheme.titleMedium,
+                  ),
+                  const Card(
+                    elevation: 0,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 16.0,
+                      ),
+                      child: RecentScoresView(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
